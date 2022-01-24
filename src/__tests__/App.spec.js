bf94d4658661acd8976fabb28d89f67d8b50ba35
mbakin <mehmetberkakinn@outlook.com>
2022-01-24 07:33:31 +0300

    feat: create `mountComponent` function for mock state and other fields

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,5 +1,24 @@
+import {createLocalVue, mount} from "@vue/test-utils";
+import App from "@/App";
+import {getters, state} from "@/store";
+import Vuex from "vuex";
+
+function mountComponent() {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  
+  return mount(App, {
+    localVue,
+    store: new Vuex.Store({
+      state,
+      getters
+    })
+  });
+}
+
 describe( 'App.vue component', () =>{
   it('sanity check', () => {
     expect(true).toBe(true);
   });
+
 })
